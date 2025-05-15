df400e04dab8240e9b90a94d0f5c83e03283aea5
TonioGoldRanger <yosoyluisarias21@gmail.com>
2025-05-14 20:30:32 -0600

    Se agregó la ventana de eventos y su respectiva conexión a la base de datos. Además se reorganizó la carpeta images y sus referencias en html y sql.

---
+++ eventos.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+$conn = new mysqli("localhost", "admin_libreria", "admin_libreria", "eq3libreria");
+
+if ($conn->connect_error) {
+    echo json_encode(["error" => "Error de conexión"]);
+    exit;
+}
+
+$sql = "
+SELECT 
+    NOMBRE,
+    DESCRIPCION,
+    RUTA_IMAGEN,
+    FECHA_INICIO,
+    FECHA_FIN
+FROM EVENTO
+ORDER BY FECHA_INICIO
+";
+
+$resultado = $conn->query($sql);
+
+$eventos = [];
+
+while ($evento = $resultado->fetch_assoc()) {
+    $eventos[] = $evento;
+}
+
+echo json_encode($eventos, JSON_UNESCAPED_UNICODE);
+?>
